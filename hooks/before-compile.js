ed465b921adc183f7136c58f855512ce851efbce
maolion <maolion.j@gmail.com>
2018-04-16 17:11:22 +0800

    Adjust build ios

--- hooks/before-compile.js
+++ hooks/before-compile.js
@@ -12,12 +12,14 @@ const {
   PLATFORM_IOS_DIR,
 } = require('./constants');
 
-const configXMLPath = Path.join(__dirname, '../config.xml');
+let configTplXMLPath = Path.join(__dirname, '../config-tpl.xml');
+let configXMLPath = Path.join(__dirname, '../config.xml');
 
 module.exports = async context => {
-  let configXML = await readXML(configXMLPath);
+  await FS.copyFileSync(configTplXMLPath, configXMLPath);
   let platforms = context.opts.platforms;
   let compileOptions = context.opts.options;
+  let configXML = await readXML(configXMLPath);
   let argv = minimist(compileOptions.argv);
   let version = argv['build-version'] ? resolveVersion(argv['build-version']) : undefined;
   let appName = configXML.widget.name[0];
@@ -34,6 +36,10 @@ module.exports = async context => {
   };
 
   let promises = [];
+
+  if (platforms.length > 1) {
+    throw new Error('Don\'t support build multiple platform');
+  }
 
   for (let platform of platforms) {
     switch (platform) {
@@ -83,10 +89,14 @@ async function preProcessAndroidPlatform(options) {
 }
 
 async function preProcessIOSPlatform(options) {
-  let {appName, displayName, isReleaseMode, version} = options;
+  let {appName, configXML, displayName, isReleaseMode, version} = options;
+
+  configXML.widget.$.id = 'com.vilicvane.wordsbaking-ios'
 
   let appInfoPlistPath = Path.join(__dirname, `../platforms/ios/${appName}/${appName}-Info.plist`);
   let appInfoPlist = await readPList(appInfoPlistPath);
+
+  let xmlBuilder = new xml2js.Builder();
 
   appInfoPlist['CFBundleDevelopmentRegion'] = 'zh-Hans';
   appInfoPlist['CFBundleDisplayName'] = displayName;
@@ -98,6 +108,7 @@ async function preProcessIOSPlatform(options) {
   }
 
   await writeFile(appInfoPlistPath, plist.build(appInfoPlist));
+  await writeFile(configXMLPath, xmlBuilder.buildObject(configXML));
 }
 
 async function updateAndroidBuildVersion(version) {
